55066f69a49910552aa1bcc57064c14b5e8f92d8
davidGBbr <davidgomesbrigido@gmail.com>
2023-11-15 17:32:18 -0300

    Middleware de autenticacao + verificacao token jwt

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,12 @@
 import { Router, Request, Response } from "express";
 import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
+import { DetailUserController } from "./controllers/user/DetailUserController";
+import { isAuthenticated } from "./middlewares/isAuthenticated";
 
 export const router = Router();
 
 //User Routes
 router.post("/users", new CreateUserController().handle);
 router.post("/session", new AuthUserController().handle);
+router.get("/me", isAuthenticated, new DetailUserController().handle);
---
+++ src/services/user/UserDetailService.ts
@@ -0,0 +1,7 @@
+import prismaClient from "../../prisma";
+
+export class UserDetailService {
+  async execute() {
+    return { ok: true };
+  }
+}
